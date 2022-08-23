cc1191c89c62ae16c30fbda357c175e1495ab57b
Joba Adewumi <jobaadewumis@gmail.com>
2022-08-23 20:07:54 +0100

    Changed permissions

--- lib/views/permission.dart
+++ lib/views/permission.dart
@@ -1,13 +1,45 @@
 import 'package:permission_handler/permission_handler.dart';
 
-Future<bool> requestPermission(Permission permission) async {
-  if (await permission.isGranted) {
-    return true;
-  } else {
-    var result = await permission.request();
-    if (result == PermissionStatus.granted) {
+// Saf safBusiness = Saf(
+//     '/storage/emulated/0/Android/media/com.whatsapp.w4b/WhatsApp Business/Media/.Statuses');
+
+// Saf saf = Saf(
+//     '/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/.Statuses/');
+
+// Future<bool> requestPermission(bool isWhatsAppBusiness) async {
+//   // print(safBusiness.);
+
+//   late bool? isBusinessGranted;
+//   late bool? isGranted;
+//   if (isWhatsAppBusiness) {
+//     isBusinessGranted =
+//         await safBusiness.getDirectoryPermission(isDynamic: true);
+//     // List<String>? paths = await safBusiness.getFilesPath();
+//     // print('hi');
+//     // print(paths);
+//   } else {
+//     isGranted = await saf.getDirectoryPermission(isDynamic: false);
+//   }
+
+//   if (isWhatsAppBusiness && isBusinessGranted != null && isBusinessGranted) {
+//     return true;
+//   } else if (!isWhatsAppBusiness && isGranted != null && isGranted) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+Future<bool> requestPermission(List<Permission> permissions) async {
+  for (var permission in permissions) {
+    if (await permission.isGranted) {
       return true;
+    } else {
+      var result = await permission.request();
+      if (result == PermissionStatus.granted) {
+        return true;
+      }
     }
+    return false;
   }
   return false;
 }
