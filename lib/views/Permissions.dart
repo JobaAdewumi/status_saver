5fc7adae1c0c902215b1b07aaee27b3ba768b2b9
Joba Adewumi <jobaadewumis@gmail.com>
2022-09-13 23:04:33 +0100

    Added logic for proper permissions

--- lib/views/Permissions.dart
+++ lib/views/Permissions.dart
@@ -1,24 +1,143 @@
+import 'package:flutter/material.dart';
+
+import 'package:saf/saf.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:saf/saf.dart';
 
-import 'package:shared_storage/media_store.dart' as mediaStore;
+import 'package:permission_handler/permission_handler.dart';
+
+import 'package:all_status_saver/routes/routes.dart' as route;
 
 // Saf safBusiness = Saf('/storage/emulated/0/Android/media/');
 
 Saf saf = Saf('/storage/emulated/0/Android/media/');
-Saf savedStatuses = Saf('/storage/emulated/0/DCIM/All Status Saver/');
+Saf savedStatuses = Saf('/storage/emulated/0/Pictures/All Status Saver/');
 
 List<String>? directoriesPaths;
 
 List<String>? savedStatusesPath;
-late bool aboveAndroid10;
+bool? aboveAndroid10;
 
-Future<bool> requestPermission11() async {
+late bool dialogForStoragePermissionGranted;
+bool dialogForAndroid11Granted = false;
+
+class Android11PermissionDialog extends StatelessWidget {
+  const Android11PermissionDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Permission'),
+      ),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(top: 20.0, bottom: 50.0),
+            child: Icon(
+              Icons.folder_outlined,
+              size: 200.0,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 10.0, right: 10.0),
+            child: Text(
+              'Starting Android 11 (Android R), \n All Status Saver can no longer access Statuses easily due to Android Storage limitations.\n',
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 10.0, right: 10.0),
+            child: Text(
+              'To access Statuses again, certain additional steps are required to be followed from your end. These are ony required to be performed once. \n',
+            ),
+          ),
+          const Text(
+            'Follow below steps:\n',
+          ),
+          const Text(
+            "1: Enable 'Show Internal Storage' if you cannot see your Primary Storage.\n",
+          ),
+          const Text(
+              '2: Select your Phones Primary Storage from the left drawer.\n'),
+          const Text("3: Go to 'Android/media' folder\n"),
+          const Text("4: Select the directory and it's done!\n"),
+          const Padding(
+            padding: EdgeInsets.only(left: 10.0, right: 10.0),
+            child: Text(
+                'Note: if the folder is already opened on the next screen, just allow the access to the same.\n'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              dialogForAndroid11Granted = true;
+              Navigator.popAndPushNamed(context, route.homePage);
+              requestPermission11(context);
+            },
+            child: const Text('Grant Folder permission'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Future showDialogForStoragePermission(context) {
+  return showDialog(
+    context: context,
+    barrierDismissible: true,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Storage Permission'),
+        content: const Text(
+            'The App needs Storage related Permissions to work Properly.\n (Example: Saving Statuses) \n\n Please Grant the Permission'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              dialogForStoragePermissionGranted = false;
+              Navigator.popAndPushNamed(context, route.homePage);
+            },
+            child: const Text('Exit'),
+          ),
+          TextButton(
+            onPressed: () {
+              dialogForStoragePermissionGranted = true;
+              Navigator.popAndPushNamed(context, route.homePage);
+            },
+            child: const Text('Grant'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+Future<bool> requestPermission11(context) async {
+  bool normalStorage = await requestPermission([Permission.storage], context);
+  directoriesPaths = await Saf.getPersistedPermissionDirectories();
+  print(directoriesPaths!.isNotEmpty);
+  // if (directoriesPaths.isEmpty) {}
+  if (directoriesPaths == null && directoriesPaths!.isNotEmpty) {
+    print('hey');
+    print(directoriesPaths);
+    if (directoriesPaths![0] == 'Android/media' && normalStorage) {
+      return true;
+    }
+  }
   // print(safBusiness.);
 
   late bool? isGranted;
+
+  print(dialogForAndroid11Granted);
+  if (!dialogForAndroid11Granted) {
+    if (!normalStorage) {
+      return false;
+    }
+    print('hi');
+
+    if (normalStorage) {
+      Navigator.popAndPushNamed(context, route.android11Screen);
+    }
+    return false;
+  }
 
   isGranted = await saf.getDirectoryPermission(isDynamic: true);
   print('value for android 11 function');
@@ -26,9 +145,6 @@ Future<bool> requestPermission11() async {
 
   if (isGranted != null && isGranted) {
     directoriesPaths = await Saf.getPersistedPermissionDirectories();
-    var trial = await mediaStore
-        .getMediaStoreContentDirectory(mediaStore.MediaStoreCollection.images);
-    print(trial);
     DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
     AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
     if (androidInfo.version.sdkInt! >= 30) {
@@ -62,38 +178,19 @@ Future<bool> requestPermissionForSavedStatus() async {
   }
 }
 
-// Future<bool> requestPermission11(bool isWhatsAppBusiness) async {
-//   // print(safBusiness.);
-
-//   late bool? isBusinessGranted;
-//   late bool? isGranted;
-//   if (isWhatsAppBusiness) {
-//     isBusinessGranted =
-//         await safBusiness.getDirectoryPermission(isDynamic: true);
-//     // List<String>? paths = await safBusiness.getFilesPath();
-//     // print('hi');
-//     // print(paths);
-//   } else {
-//     isGranted = await saf.getDirectoryPermission(isDynamic: false);
-//   }
-
-//   if (isWhatsAppBusiness && isBusinessGranted != null && isBusinessGranted) {
-//     return true;
-//   } else if (!isWhatsAppBusiness && isGranted != null && isGranted) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-
-Future<bool> requestPermission(List<Permission> permissions) async {
+Future<bool> requestPermission(List<Permission> permissions, context) async {
   for (var permission in permissions) {
     if (await permission.isGranted) {
       return true;
     } else {
-      var result = await permission.request();
-      if (result == PermissionStatus.granted) {
-        return true;
+      await showDialogForStoragePermission(context);
+      print(dialogForStoragePermissionGranted);
+      if (dialogForStoragePermissionGranted) {
+        var result = await permission.request();
+        if (result == PermissionStatus.granted) {
+          return true;
+        }
+        return false;
       }
     }
     return false;
