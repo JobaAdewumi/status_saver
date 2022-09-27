fce25136e82cd0389ca447e71358a75bc08bad20
Joba Adewumi <jobaadewumis@gmail.com>
2022-09-27 01:18:39 +0100

    Cleaned up code and refactored

--- lib/views/WhatsAppViews/home/Home.dart
+++ lib/views/WhatsAppViews/home/Home.dart
@@ -1,9 +1,8 @@
 import 'package:all_status_saver/helpers/storage_manager.dart';
 import 'package:device_info_plus/device_info_plus.dart';
-import 'package:flutter/material.dart';
-import 'package:saf/saf.dart';
 
 import 'package:all_status_saver/routes/routes.dart' as route;
+import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 import 'package:all_status_saver/views/Permissions.dart';
@@ -18,11 +17,9 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
   bool? aboveAndroid10;
   int? androidVersion;
-
-  Saf safBusiness = Saf(
-      '/storage/emulated/0/Android/media/com.whatsapp.w4b/WhatsApp Business/Media/.Statuses');
 
   List<Permission> permissionsNeeded = [
     Permission.storage,
@@ -79,16 +76,174 @@ class HomePageState extends State<HomePage> {
     }
   }
 
+  Widget homeBody() {
+    var size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15, right: 15, top: 10),
+            child: Column(
+              children: [
+                Container(
+                  width: size.width,
+                  height: 55,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: Theme.of(context).appBarTheme.backgroundColor,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      right: 20,
+                      left: 20,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            _drawerKey.currentState!.openDrawer();
+                          },
+                          splashColor: Colors.grey,
+                          child: Icon(
+                            Icons.menu,
+                            size: 25,
+                            color: Theme.of(context).iconTheme.color,
+                          ),
+                        ),
+                        Text(
+                          'Status Saver',
+                          style: TextStyle(
+                            color: Theme.of(context)
+                                .primaryTextTheme
+                                .headline1!
+                                .color,
+                            fontSize: 23,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        SizedBox(width: 20, child: Container()),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: GestureDetector(
+                        onTap: () async {
+                          await permissionHandler(context, route.whatsAppPage);
+                        },
+                        child: Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Column(
+                              children: const [
+                                Padding(
+                                  padding: EdgeInsets.only(bottom: 5.0),
+                                  child: Icon(
+                                    Icons.whatsapp,
+                                    size: 60.0,
+                                  ),
+                                ),
+                                Text('WA STATUS'),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: GestureDetector(
+                        onTap: () async {
+                          await permissionHandler(context, route.whatsappBPage);
+                        },
+                        child: Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Column(
+                              children: const [
+                                Padding(
+                                  padding: EdgeInsets.only(bottom: 5.0),
+                                  child: FlutterLogo(
+                                    size: 60.0,
+                                  ),
+                                ),
+                                Text('WB STATUS'),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                androidVersion! >= 30
+                    ? Container()
+                    : Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton.icon(
+                            icon: Icon(Icons.save_alt_outlined,
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .bodyText2!
+                                    .color),
+                            onPressed: () async {
+                              await permissionHandler(
+                                  context, route.savedStatusPage);
+                            },
+                            label: Text(
+                              'Saved Statuses',
+                              style: TextStyle(
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .bodyText2!
+                                      .color),
+                            ),
+                          ),
+                          // ElevatedButton.icon(
+                          //   icon: const Icon(Icons.bug_report),
+                          //   onPressed: () async {
+                          //     if (await requestPermission(Permission.storage)) {
+                          //       Navigator.pushNamed(context, route.introScreen);
+                          //     } else {
+                          //       return;
+                          //     }
+                          //   },
+                          //   label: const Text('Intro Screen debug'),
+                          // ),
+                        ],
+                      ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () => showExitPopup(context),
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Status Saver'),
-        ),
+        key: _drawerKey,
         drawer: Drawer(
           width: 280.0,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
           child: ListView(
             children: [
               DrawerHeader(
@@ -130,93 +285,7 @@ class HomePageState extends State<HomePage> {
             ],
           ),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(5),
-                    child: GestureDetector(
-                      onTap: () async {
-                        await permissionHandler(context, route.whatsAppPage);
-                      },
-                      child: Card(
-                        child: Padding(
-                          padding: const EdgeInsets.all(20),
-                          child: Column(
-                            children: const [
-                              Padding(
-                                padding: EdgeInsets.only(bottom: 5.0),
-                                child: Icon(
-                                  Icons.whatsapp,
-                                  size: 60.0,
-                                ),
-                              ),
-                              Text('WA STATUS'),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(5),
-                    child: GestureDetector(
-                      onTap: () async {
-                        await permissionHandler(context, route.whatsappBPage);
-                      },
-                      child: Card(
-                        child: Padding(
-                          padding: const EdgeInsets.all(20),
-                          child: Column(
-                            children: const [
-                              Padding(
-                                padding: EdgeInsets.only(bottom: 5.0),
-                                child: FlutterLogo(
-                                  size: 60.0,
-                                ),
-                              ),
-                              Text('WB STATUS'),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              androidVersion! >= 30
-                  ? Container()
-                  : Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ElevatedButton.icon(
-                          icon: const Icon(Icons.save_alt_outlined),
-                          onPressed: () async {
-                            await permissionHandler(
-                                context, route.savedStatusPage);
-                          },
-                          label: const Text('Saved Statuses'),
-                        ),
-                        // ElevatedButton.icon(
-                        //   icon: const Icon(Icons.bug_report),
-                        //   onPressed: () async {
-                        //     if (await requestPermission(Permission.storage)) {
-                        //       Navigator.pushNamed(context, route.introScreen);
-                        //     } else {
-                        //       return;
-                        //     }
-                        //   },
-                        //   label: const Text('Intro Screen debug'),
-                        // ),
-                      ],
-                    ),
-            ],
-          ),
-        ),
+        body: homeBody(),
       ),
     );
   }
