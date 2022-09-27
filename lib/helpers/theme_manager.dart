f33ae01be5343b244ae02397992f4e46e6e8555f
Joba Adewumi <jobaadewumis@gmail.com>
2022-09-27 01:17:46 +0100

    Added new colors and material 3

--- lib/helpers/theme_manager.dart
+++ lib/helpers/theme_manager.dart
@@ -3,7 +3,21 @@ import 'package:flutter/material.dart';
 
 class ThemeNotifier with ChangeNotifier {
   static const MaterialColor darkSwatch =
-      MaterialColor(0xFF009688, <int, Color>{
+      MaterialColor(0xFF25434F, <int, Color>{
+    50: Color(0xFFE0F2F1),
+    100: Color(0xFFB2DFDB),
+    200: Color(0xFF80CBC4),
+    300: Color(0xFF4DB6AC),
+    400: Color(0xFF26A69A),
+    500: Color(0xFF009688),
+    600: Color(0xFF00897B),
+    700: Color(0xFF00796B),
+    800: Color(0xFF00695C),
+    900: Color(0xFF004D40),
+  });
+
+  static const MaterialColor lightSwatch =
+      MaterialColor(0xFF99CDD1, <int, Color>{
     50: Color(0xFFE0F2F1),
     100: Color(0xFFB2DFDB),
     200: Color(0xFF80CBC4),
@@ -19,6 +33,14 @@ class ThemeNotifier with ChangeNotifier {
   final darkTheme = ThemeData(
     dividerColor: Colors.white54,
     primarySwatch: darkSwatch,
+    useMaterial3: true,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color.fromARGB(255, 37, 67, 79),
+    ),
+    dialogTheme: const DialogTheme(
+      backgroundColor: Color.fromARGB(255, 35, 41, 46),
+    ),
+    scaffoldBackgroundColor: const Color.fromARGB(255, 35, 41, 46),
     primaryColorDark: Colors.black,
     brightness: Brightness.dark,
     backgroundColor: Colors.black,
@@ -27,13 +49,21 @@ class ThemeNotifier with ChangeNotifier {
   );
 
   final lightTheme = ThemeData(
-    dividerColor: Colors.black54,
-    primarySwatch: darkSwatch,
-    // accentColor: Colors.black,
-    primaryColorDark: Colors.white,
-    brightness: Brightness.light,
-    backgroundColor: const Color(0xFFE5E5E5),
-  );
+      dividerColor: Colors.black54,
+      primarySwatch: lightSwatch,
+      useMaterial3: true,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Color.fromARGB(255, 172, 200, 202),
+      ),
+      dialogTheme: const DialogTheme(
+        backgroundColor: Color.fromARGB(255, 153, 205, 209),
+      ),
+      scaffoldBackgroundColor: const Color.fromARGB(255, 203, 212, 212),
+      // accentColor: Colors.black,
+      brightness: Brightness.light,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: const Color.fromARGB(255, 153, 205, 209),
+      ));
 
   late ThemeData _themeData = lightTheme;
   late String _currentTheme = 'light';
