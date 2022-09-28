91fdfe764356e2c25e7decbd4883831e844979c6
Joba Adewumi <jobaadewumis@gmail.com>
2022-09-28 02:16:11 +0100

    Merged save status page with whatsapp and wb

--- lib/views/WhatsAppViews/whatsapp/Whatsapp.dart
+++ lib/views/WhatsAppViews/whatsapp/Whatsapp.dart
@@ -3,14 +3,26 @@ import 'dart:io';
 import 'package:all_status_saver/functions/global_functions.dart';
 import 'package:all_status_saver/helpers/storage_manager.dart';
 import 'package:all_status_saver/views/Permissions.dart';
-import 'package:all_status_saver/widgets/grid-views.dart';
+import 'package:all_status_saver/views/WhatsAppViews/home/Viewer.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:file_manager/file_manager.dart';
 import 'package:flutter/material.dart';
 
-class WhatsApp extends StatefulWidget {
+import 'package:all_status_saver/routes/routes.dart' as route;
+
+class WhatsAppOptions {
   final bool isWhatsApp;
-  const WhatsApp({super.key, required this.isWhatsApp});
+  final bool isStatusPage;
+
+  WhatsAppOptions({
+    required this.isWhatsApp,
+    this.isStatusPage = false,
+  });
+}
+
+class WhatsApp extends StatefulWidget {
+  final WhatsAppOptions whatsAppOptions;
+  const WhatsApp({super.key, required this.whatsAppOptions});
 
   @override
   State<WhatsApp> createState() => _WhatsAppState();
@@ -82,6 +94,332 @@ class _WhatsAppState extends State<WhatsApp> with TickerProviderStateMixin {
     );
   }
 
+  Widget ImageGrid(
+    File statuses,
+    List<FileType> files,
+    int index,
+    BuildContext context,
+  ) {
+    Widget image = InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, route.viewer,
+            arguments:
+                MultimediaViewer(isImage: true, allFiles: files, index: index));
+      },
+      child: Image.file(statuses, height: 100, width: 100, fit: BoxFit.cover),
+    );
+    return Card(
+      child: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: SizedBox(
+              width: 350,
+              height: 134,
+              child: image,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                        fixedSize: const Size(60.0, 53.0), primary: Colors.red),
+                    onPressed: () async {
+                      await GlobalFunctions().shareFile(statuses.path);
+                    },
+                    child: Column(
+                      children: const [
+                        Icon(Icons.share),
+                        Text(
+                          'SHARE',
+                          style: TextStyle(fontSize: 10),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                        fixedSize: const Size(60.0, 53.0),
+                        primary: Colors.green),
+                    onPressed: () async {
+                      await GlobalFunctions().shareFile(statuses.path);
+                    },
+                    child: Column(
+                      children: const [
+                        Icon(Icons.cached),
+                        Text(
+                          'REPOST',
+                          style: TextStyle(fontSize: 10),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                widget.whatsAppOptions.isStatusPage
+                    ? Expanded(
+                        flex: 1,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                              fixedSize: const Size(60.0, 53.0),
+                              primary: Colors.red),
+                          onPressed: () async {
+                            return showDialog(
+                              context: context,
+                              barrierDismissible: true,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: const Text('Delete Permanently?'),
+                                  content: const Text(
+                                      'Are you sure you want to delete this file permanently?'),
+                                  contentPadding: const EdgeInsets.fromLTRB(
+                                      24.0, 20.0, 24.0, 17.0),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: const Text('Cancel'),
+                                    ),
+                                    TextButton(
+                                      onPressed: () async {
+                                        setState(() {
+                                          statuses.deleteSync();
+                                        });
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: const Text('Delete'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                          child: Column(
+                            children: const [
+                              Icon(Icons.delete_forever_rounded),
+                              Text(
+                                'DELETE',
+                                style: TextStyle(fontSize: 10),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    : Expanded(
+                        flex: 1,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            fixedSize: const Size(60.0, 53.0),
+                            primary: Colors.blue,
+                          ),
+                          onPressed: () async {
+                            await GlobalFunctions()
+                                .saveStatus(statuses.path, context);
+                          },
+                          child: Column(
+                            children: const [
+                              Icon(Icons.save_alt),
+                              Text(
+                                'SAVE',
+                                style: TextStyle(fontSize: 10),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget VideoGrid(
+    List<FileType> allStatuses,
+    File statuses,
+    int index,
+    BuildContext context,
+  ) {
+    return FutureBuilder(
+      future: GlobalFunctions().generateVideoThumbnail(statuses),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          Image video = Image.memory(snapshot.data as dynamic,
+              height: 100, width: 100, fit: BoxFit.cover);
+          return Card(
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Stack(
+                    children: [
+                      SizedBox(
+                        width: 350,
+                        height: 134,
+                        child: video,
+                      ),
+                      Positioned(
+                        top: 30,
+                        left: 60,
+                        child: IconButton(
+                          onPressed: () {
+                            Navigator.pushNamed(
+                              context,
+                              route.viewer,
+                              arguments: MultimediaViewer(
+                                  allFiles: allStatuses, index: index),
+                            );
+                          },
+                          icon: const Icon(
+                            Icons.play_circle_outline,
+                            color: Colors.white,
+                            size: 50,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                              fixedSize: const Size(60.0, 53.0),
+                              primary: Colors.red),
+                          onPressed: () async {
+                            await GlobalFunctions().shareFile(statuses.path);
+                          },
+                          child: Column(
+                            children: const [
+                              Icon(Icons.share),
+                              Text(
+                                'SHARE',
+                                style: TextStyle(fontSize: 10),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                              fixedSize: const Size(60.0, 53.0),
+                              primary: Colors.green),
+                          onPressed: () async {
+                            await GlobalFunctions().shareFile(statuses.path);
+                          },
+                          child: Column(
+                            children: const [
+                              Icon(Icons.cached),
+                              Text(
+                                'REPOST',
+                                style: TextStyle(fontSize: 10),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      widget.whatsAppOptions.isStatusPage
+                          ? Expanded(
+                              flex: 1,
+                              child: TextButton(
+                                style: TextButton.styleFrom(
+                                    fixedSize: const Size(60.0, 53.0),
+                                    primary: Colors.red),
+                                onPressed: () async {
+                                  return showDialog(
+                                    context: context,
+                                    barrierDismissible: true,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title:
+                                            const Text('Delete Permanently?'),
+                                        content: const Text(
+                                            'Are you sure you want to delete this file permanently?'),
+                                        contentPadding:
+                                            const EdgeInsets.fromLTRB(
+                                                24.0, 20.0, 24.0, 17.0),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: const Text('Cancel'),
+                                          ),
+                                          TextButton(
+                                            onPressed: () async {
+                                              setState(() {
+                                                statuses.deleteSync();
+                                              });
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: const Text('Delete'),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                                child: Column(
+                                  children: const [
+                                    Icon(Icons.delete_forever_rounded),
+                                    Text(
+                                      'DELETE',
+                                      style: TextStyle(fontSize: 10),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )
+                          : Expanded(
+                              flex: 1,
+                              child: TextButton(
+                                style: TextButton.styleFrom(
+                                  fixedSize: const Size(60.0, 53.0),
+                                  primary: Colors.blue,
+                                ),
+                                onPressed: () async {
+                                  await GlobalFunctions()
+                                      .saveStatus(statuses.path, context);
+                                },
+                                child: Column(
+                                  children: const [
+                                    Icon(Icons.save_alt),
+                                    Text(
+                                      'SAVE',
+                                      style: TextStyle(fontSize: 10),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+
   Widget AllStatuses(List<FileType> gImagesVideo) {
     Widget imagesVideos;
     var checkTypes = gImagesVideo.where(
@@ -115,14 +453,14 @@ class _WhatsAppState extends State<WhatsApp> with TickerProviderStateMixin {
             if (entit.file.path.contains('.jpg') ||
                 entit.file.path.contains('.jpeg') ||
                 entit.file.path.contains('.png')) {
-              return GlobalWidgets().ImageGrid(
+              return ImageGrid(
                 statuses,
                 gImagesVideo,
                 index,
                 context,
               );
             }
-            return GlobalWidgets().VideoGrid(
+            return VideoGrid(
               gImagesVideo,
               statuses,
               index,
@@ -132,7 +470,9 @@ class _WhatsAppState extends State<WhatsApp> with TickerProviderStateMixin {
         ),
       );
     } else {
-      imagesVideos = GlobalFunctions().noStatusError(true);
+      imagesVideos = GlobalFunctions().noStatusError(
+          widget.whatsAppOptions.isWhatsApp,
+          widget.whatsAppOptions.isStatusPage);
     }
     return imagesVideos;
   }
@@ -164,12 +504,14 @@ class _WhatsAppState extends State<WhatsApp> with TickerProviderStateMixin {
               entit.file.delete();
             }
 
-            return GlobalWidgets().VideoGrid(gVideos, statuses, index, context);
+            return VideoGrid(gVideos, statuses, index, context);
           },
         ),
       );
     } else {
-      videos = GlobalFunctions().noStatusError(true);
+      videos = GlobalFunctions().noStatusError(
+          widget.whatsAppOptions.isWhatsApp,
+          widget.whatsAppOptions.isStatusPage);
     }
     return videos;
   }
@@ -203,12 +545,14 @@ class _WhatsAppState extends State<WhatsApp> with TickerProviderStateMixin {
               entit.file.delete();
             }
 
-            return GlobalWidgets().ImageGrid(statuses, gImages, index, context);
+            return ImageGrid(statuses, gImages, index, context);
           },
         ),
       );
     } else {
-      images = GlobalFunctions().noStatusError(true);
+      images = GlobalFunctions().noStatusError(
+          widget.whatsAppOptions.isWhatsApp,
+          widget.whatsAppOptions.isStatusPage);
     }
     return images;
   }
@@ -244,13 +588,15 @@ class _WhatsAppState extends State<WhatsApp> with TickerProviderStateMixin {
 
             if (androidVersion! <= 29) {
               if (entities.isEmpty) {
-                return GlobalFunctions().noStatusError(true);
+                return GlobalFunctions().noStatusError(
+                    widget.whatsAppOptions.isWhatsApp,
+                    widget.whatsAppOptions.isStatusPage);
               }
             }
 
             FileSystemEntity? entity;
             FileSystemEntity? entity11;
-            if (widget.isWhatsApp) {
+            if (widget.whatsAppOptions.isWhatsApp) {
               for (var e in entities) {
                 if (FileManager.basename(e) == 'WhatsApp') {
                   entity = e;
@@ -259,7 +605,8 @@ class _WhatsAppState extends State<WhatsApp> with TickerProviderStateMixin {
                   entity11 = e;
                 }
               }
-            } else if (!widget.isWhatsApp) {
+            } else if (!widget.whatsAppOptions.isWhatsApp &&
+                !widget.whatsAppOptions.isStatusPage) {
               for (var e in entities) {
                 if (FileManager.basename(e) == 'WhatsApp Business') {
                   entity = e;
@@ -268,24 +615,35 @@ class _WhatsAppState extends State<WhatsApp> with TickerProviderStateMixin {
                   entity11 = e;
                 }
               }
+            } else if (widget.whatsAppOptions.isStatusPage) {
+              for (var e in entities) {
+                if (FileManager.basename(e) == 'DCIM') {
+                  entity = e;
+                }
+              }
             }
 
             var directoryPath = directoriesPaths?[0];
 
             String? path = entity?.path ?? '';
-            String newPath = '$path/Media/.Statuses/';
+            String newPath = widget.whatsAppOptions.isStatusPage
+                ? '$path/All Status Saver/'
+                : '$path/Media/.Statuses/';
             entity = Directory(newPath);
 
             String? path11 = directoryPath ?? 'Android/media';
 
-            String newPath11 = widget.isWhatsApp
+            String newPath11 = widget.whatsAppOptions.isWhatsApp
                 ? '/storage/emulated/0/$path11/com.whatsapp/WhatsApp/Media/.Statuses'
                 : '/storage/emulated/0/$path11/com.whatsapp.w4b/WhatsApp Business/Media/.Statuses';
+            print(newPath11);
 
             entity11 = Directory(newPath11);
 
             if (!entity.existsSync() && !entity11.existsSync()) {
-              return GlobalFunctions().noStatusError(true);
+              return GlobalFunctions().noStatusError(
+                  widget.whatsAppOptions.isWhatsApp,
+                  widget.whatsAppOptions.isStatusPage);
             }
 
             globalStatusPath = newPath;
