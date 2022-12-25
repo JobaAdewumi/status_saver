db9ee00a7e9cf25e4c64e9d39bb3551e7d9447c7
Joba Adewumi <jobaadewumis@gmail.com>
2022-12-25 08:39:13 +0100

    Added Flutter logs

--- lib/common_libs.dart
+++ lib/common_libs.dart
@@ -4,3 +4,4 @@ export 'package:all_status_saver/logic/settings_logic.dart';
 export 'package:all_status_saver/logic/whatsapp_logic.dart';
 export 'package:all_status_saver/main.dart';
 export 'package:all_status_saver/routes/routes.dart';
+export 'package:flutter_logs/flutter_logs.dart';
