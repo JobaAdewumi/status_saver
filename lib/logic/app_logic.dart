09673b5fa6d46137d588b67bf4d9ce9803c8e8aa
Joba Adewumi <jobaadewumis@gmail.com>
2022-12-30 02:52:47 +0100

    Added Flutter logs initialization

--- lib/logic/app_logic.dart
+++ lib/logic/app_logic.dart
@@ -9,9 +9,11 @@ class AppLogic {
   int? androidVersion;
 
   Future<void> bootstrap() async {
-    FlutterError.onError = _handleFlutterError;
+    await _flutterLogsInit();
 
     await _getPlatformDetails();
+
+    FlutterError.onError = _handleFlutterError;
 
     await settingsLogic.loadTheme();
 
@@ -22,6 +24,42 @@ class AppLogic {
 
   void _handleFlutterError(FlutterErrorDetails details) {
     FlutterError.dumpErrorToConsole(details);
+    FlutterLogs.logThis(
+        tag: 'Init Error',
+        subTag: 'Flutter error',
+        logMessage: 'Flutter error during initialization',
+        errorMessage: 'Error thrown during handle flutter error initialization',
+        level: LogLevel.SEVERE);
+  }
+
+  Future _flutterLogsInit() async {
+    await FlutterLogs.initLogs(
+      logLevelsEnabled: [
+        LogLevel.INFO,
+        LogLevel.WARNING,
+        LogLevel.ERROR,
+        LogLevel.SEVERE
+      ],
+      timeStampFormat: TimeStampFormat.TIME_FORMAT_READABLE,
+      directoryStructure: DirectoryStructure.FOR_DATE,
+      logTypesEnabled: [
+        'device',
+        'network',
+        'errors',
+      ],
+      logFileExtension: LogFileExtension.LOG,
+      logsWriteDirectoryName: 'DebugLogs',
+      logsExportDirectoryName: 'Debug/Exported',
+      debugFileOperations: true,
+      isDebuggable: true,
+      logSystemCrashes: true,
+    );
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+    FlutterLogs.logWarn(
+        'Device Info',
+        "Device model - ${androidInfo.model ?? ''} \n deviceId - ${androidInfo.id ?? ''} \n deviceBrand - ${androidInfo.brand ?? ''} \n deviceManufacturer - ${androidInfo.manufacturer ?? ''} \n deviceSdkInt - ${androidInfo.version.sdkInt.toString()}",
+        'My Log Message');
   }
 
   Future _getPlatformDetails() async {
