4b351493ee464a65ed52fd6595c7c18a9dafef4a
Joba Adewumi <jobaadewumis@gmail.com>
2023-07-16 19:32:12 +0100

    Updated engine and dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,14 @@
 import FlutterMacOS
 import Foundation
 
-import device_info_plus_macos
-import path_provider_macos
-import share_plus_macos
-import shared_preferences_macos
-import url_launcher_macos
+import device_info_plus
+import path_provider_foundation
+import share_plus
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
